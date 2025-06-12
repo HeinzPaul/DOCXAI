b477c6bb170779c3f29b38f092f6afe698ec0537
HeinzPaul <heinzbinjupaul@gmail.com>
2025-06-12 09:18:38 +0530

    Added basic metadata for .docx files

--- main.py
+++ main.py
@@ -11,28 +11,28 @@ def extract_text_from_docx(docx_path):
     for para in doc.paragraphs:
         if para.text.strip():
             text += para.text + "\n"
-    text_as_doc = Document(page_content = text)
+    text_as_doc = Document(page_content=text, metadata={"source": os.path.basename(docx_path), "file_type": "docx"})
     return [text_as_doc]
 
 
-def extractor(pdf_path):
-    if os.path.exists(pdf_path):
-        if pdf_path.lower().endswith(".pdf"):
+def extractor(file_path):
+    if os.path.exists(file_path):
+        if file_path.lower().endswith(".pdf"):
 
-            loader = PDFPlumberLoader(pdf_path) # we load the file
+            loader = PDFPlumberLoader(file_path) # we load the file
             pages = loader.load() # we load it into documnent object
 
-            print("Sucessfully loaded",len(pages), "from",pdf_path)
+            print("Sucessfully loaded",len(pages), "from",file_path)
             return pages
-        elif pdf_path.lower().endswith(".docx"):
-            text = extract_text_from_docx(pdf_path)
+        elif file_path.lower().endswith(".docx"):
+            text = extract_text_from_docx(file_path)
             return text
 
     else:
         print("Path does not exist")
 
-pdf_path = input("Enter the name of the path")
-pages = extractor(pdf_path)
+file_path = input("Enter the name of the path")
+pages = extractor(file_path)
 print(pages)
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=200)
 chunks = text_splitter.split_documents(pages)
